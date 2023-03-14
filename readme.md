dc4f82ad02a73d05ba90cefdf802591476b94d59
c-kiplimo <kiplimocollins855@gmail.com>
2023-03-15 02:21:02 +0300

    implemented  push and pop

---
+++ readme.md
@@ -0,0 +1,5 @@
+## Stacks
++ Stack --- LIFO data structure Last-In-First-Out
++ Stores  objects into a sort of "vertical tower" 
++ push() to add to the top 
++ pop() to remove from the top
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Stack;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        Stack<String> stack = new Stack<String>();
+        stack.push("Minecraft");
+        stack.push("Skyrim");
+        stack.push("BorderLands");
+        stack.push("DOOM");
+        stack.push("FVII");
+
+//        System.out.println(stack.peek());
+//        String myFavGame =stack.pop();
+        System.out.println(stack.search("DOOM"));
+//        System.out.println(myFavGame);
+//        for (int i=0; i < 1000000000;i++){
+//            stack.push("Fallout76");
+//        }
+
     }
 }
